22c18c55eef811be27c89d37a6eeb32a91e3593e
Thomas Lind <thompa@Thomass-MacBook-Air.local>
2018-03-21 00:09:28 +0100

    commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,13 +4,16 @@ import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
+import { NativeScriptHttpModule } from "nativescript-angular/http";
+
 @NgModule({
     bootstrap: [
         AppComponent
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NativeScriptHttpModule
     ],
     declarations: [
         AppComponent
--- app/tabs/home/home.component.ts
+++ app/tabs/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from "@angular/core";
 
+import { Router } from "@angular/router";
+import { Http, Headers, RequestOptions } from "@angular/http";
+import { Observable as RxObservable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/do";
+
+
 @Component({
     selector: "Home",
     moduleId: module.id,
     templateUrl: "./home.component.html"
 })
 export class HomeComponent implements OnInit {
-    constructor() {
+    constructor(
+        private http: Http, private router: Router
+    ) {
         /* ***********************************************************
         * Use the constructor to inject services.
         *************************************************************/
@@ -17,4 +26,20 @@ export class HomeComponent implements OnInit {
         * Use the "ngOnInit" handler to initialize data for the view.
         *************************************************************/
     }
+
+    public login(username: string, password: string) {
+        let headers = new Headers({ "Content-Type": "application/json" });
+        let options = new RequestOptions({ headers: headers });
+        console.log('login');
+        this.http.post("https://apps.lib.kth.se/webservices/mrbs/api/v1/entries", JSON.stringify({ username: username, password: password }), options)
+            .map(result => result.json())
+            .subscribe(result => {
+                console.log(result);
+                this.router.navigate(["game"], { queryParams: { jwt: result.token } });
+            }, error => {
+                //Toast.makeText(error.json().message).show();
+                console.log(error.json().message);
+            });
+    }
+    
 }
